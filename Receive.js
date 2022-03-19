e38378a21f57ba698535dc525e90b1db87fc1118
Anuj Santosh Barave <80421130+anujbarave99@users.noreply.github.com>
2022-03-19 10:04:11 +0530

    10march22-10:04

--- Receive.js
+++ Receive.js
@@ -1,34 +1,66 @@
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({region: 'REGION'});
+var AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({
+  region: "ap-south-1",
+});
 
 // Create an SQS service object
-var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var queueURL = "https://sqs.ap-south-1.amazonaws.com/000263543451/TestQueue";
 
 var params = {
-   // Remove DelaySeconds parameter and value for FIFO queues
-  DelaySeconds: 10,
-  MessageAttributes: {
-    "ID": {
-      DataType: "Number",
-      StringValue: "67"
-    },
-    "Name": {
-      DataType: "String",
-      StringValue: "prasad barave"
-    }
-  },
-  MessageBody: "Test Message",
-  // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-  // MessageGroupId: "Group1",  // Required for FIFO queues
-  QueueUrl: "https://sqs.ap-south-1.amazonaws.com/000263543451/TestQueue"
+  AttributeNames: ["SentTimestamp"],
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: ["All"],
+  QueueUrl: queueURL,
+  VisibilityTimeout: 20,
+  WaitTimeSeconds: 0,
 };
 
-sqs.sendMessage(params, function(err, data) {
+sqs.receiveMessage(params, function (err, data) {
   if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.MessageId);
+    console.log("Receive Error", err);
+  } else if (data.Messages) {
+    var id =  data.Messages[0].MessageAttributes.ID.StringValue;
+    var name = data.Messages[0].MessageAttributes.Name.StringValue;
+
+    var table = "Cloudoea";
+    var ID = parseInt(id);
+    var Name = name;
+
+    var params1 = {
+      TableName:table,
+      Item:{
+          "ID": ID,
+          "Name": Name   
+      }
+  };
+
+    console.log("Adding a new item...");
+    docClient.put(params1, function (err, data) {
+      if (err) {
+        console.error(
+          "Unable to add item. Error JSON:",
+          JSON.stringify(err, null, 2)
+        );
+      } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+      }
+    });
+
+    var deleteParams = {
+      QueueUrl: queueURL,
+      ReceiptHandle: data.Messages[0].ReceiptHandle,
+    };
+    sqs.deleteMessage(deleteParams, function (err, data) {
+      if (err) {
+        console.log("Delete Error", err);
+      } else {
+        console.log("Message Deleted", data);
+      }
+    });
   }
 });
